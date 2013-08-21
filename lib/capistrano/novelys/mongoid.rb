576dad8747c60340cb09df14dc2d875cf4a75c44
Kevin Sołtysiak <kevin@ksol.fr>
2013-08-21 18:52:18 +0200

    Correct paths

--- lib/capistrano/novelys/mongoid.rb
+++ lib/capistrano/novelys/mongoid.rb
@@ -5,12 +5,12 @@ after 'deploy:update_code', 'mongoid:symlink'
 namespace :mongoid do
   desc "Copy mongoid config"
   task :copy do
-    upload "config/mongoid.yml", "#{shared_path}/mongoid.yml", :via => :scp
+    upload "config/mongoid.yml", "#{shared_path}/config/mongoid.yml", :via => :scp
   end
 
   desc "Link the mongoid config in the release_path"
   task :symlink do
-    run "test -f #{release_path}/config/mongoid.yml || ln -s #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
+    run "test -f #{release_path}/config/mongoid.yml || ln -s #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
   end
 
   desc "Create MongoDB indexes"
--- lib/capistrano/novelys/s3.rb
+++ lib/capistrano/novelys/s3.rb
@@ -5,11 +5,11 @@ after 'deploy:update_code', 's3:symlink'
 namespace :s3 do
   desc "Copy amazon S3 config"
   task :copy do
-    upload "config/amazon_s3.yml", "#{shared_path}/amazon_s3.yml", :via => :scp
+    upload "config/amazon_s3.yml", "#{shared_path}/config/amazon_s3.yml", :via => :scp
   end
 
   desc "Link the config/amazon_s3.yml file in the release_path"
   task :symlink do
-    run "test -f #{release_path}/config/amazon_s3.yml || ln -s #{shared_path}/amazon_s3.yml #{release_path}/config/amazon_s3.yml"
+    run "test -f #{release_path}/config/amazon_s3.yml || ln -s #{shared_path}/config/amazon_s3.yml #{release_path}/config/amazon_s3.yml"
   end
 end
