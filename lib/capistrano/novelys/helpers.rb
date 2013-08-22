af7ba6370f783a40663e72ebc175e56febb66050
Kevin Sołtysiak <kevin@ksol.fr>
2013-08-22 17:26:25 +0200

    Refactored helpers

--- lib/capistrano/novelys/helpers.rb
+++ lib/capistrano/novelys/helpers.rb
@@ -3,11 +3,11 @@ module Capistrano
     module Helpers
       # Run rake on the server.
       # To be used in tasks.
-      def run_remote_rake(rake_cmd)
+      def run_remote_rake(cap, rake_cmd)
         rake_args = ENV['RAKE_ARGS'].to_s.split(',')
-        release = Capistrano::Configuration.instance.fetch(:latest_release)
-        rake = Capistrano::Configuration.instance.fetch(:rake, 'rake')
-        env = Capistrano::Configuration.instance.fetch(:rails_env, 'production')
+        release = cap.fetch(:latest_release)
+        rake = cap.fetch(:rake, 'rake')
+        env = cap.fetch(:rails_env, 'production')
 
         cmd = "cd #{release} && #{rake} RAILS_ENV=#{env} #{rake_cmd}"
         cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
