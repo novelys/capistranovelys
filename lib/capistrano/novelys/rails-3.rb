1c59f91e8714c99f1ac0b9dee23dbc9c7e9aa667
Kevin Sołtysiak <kevin@ksol.fr>
2013-08-22 16:40:34 +0200

    Fix #1, Rails version specific recipes

---
+++ lib/capistrano/novelys/rails-3.rb
@@ -0,0 +1,35 @@
+require 'readline'
+
+## Hooks
+after 'deploy:update_code', 'rails3:secret_token:symlink'
+
+## Tasks
+namespace :rails3 do
+  namespace :secret_token do
+    desc "Copy the secret token file to the server"
+    task :copy do
+      upload "config/initializers/secret_token.rb", "#{shared_path}/config/initializes/secret_token.rb", :via => :scp
+    end
+
+    desc "Symlink the secret token file in the current release"
+    task :symlink do
+      run "test -f #{release_path}/initializers/secret_token.rb || ln -s #{shared_path}/initializers/secret_token.rb #{release_path}/initializers/session_store.rb"
+    end
+  end
+
+  desc "Open a Rails console"
+  task :console do
+    input = ''
+    run "cd #{current_path} && #{bundle_cmd} exec rails console #{rails_env}", :once => true do |chan, stream, data|
+      next if data.chomp == input.chomp || data.chomp == ''
+      print data
+      next unless data.chomp =~ />\s+$/ # Bad prompt detection
+
+      # Readline's quite tolerant, but most control codes (Ctrl-u, Ctrl-l, ...)
+      # will screw up the process' stream.
+      input = Readline.readline('', true)
+      break if input.nil? # EOF
+      chan.send_data "#{input}\n"
+    end
+  end
+end
