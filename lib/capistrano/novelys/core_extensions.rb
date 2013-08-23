eb83e6fbf54513d9dca1ae72fcc93f2ba1a3ebf0
Kevin Sołtysiak <kevin@ksol.fr>
2013-08-23 09:34:05 +0200

    helper for using default stack

--- lib/capistrano/novelys/core_extensions.rb
+++ lib/capistrano/novelys/core_extensions.rb
@@ -1,7 +1,14 @@
 module Capistrano
   class Configuration
+    # Sugar for loading given recipes
     def use_stack(*args)
       args.each { |recipes| load "novelys/#{recipes.to_s}" }
+    end
+
+    # Sugar for loading common recipes + supplied
+    def use_default_stack_and(*args)
+      args = ([:airbrake, :logs, :rbenv, :remote_commands, :unicorn] + args).uniq
+      use_stack(*args)
     end
   end
 end
--- lib/capistrano/novelys/version.rb
+++ lib/capistrano/novelys/version.rb
@@ -1,5 +1,5 @@
 module Capistrano
   module Novelys
-    VERSION = '0.2.1'
+    VERSION = '0.3.0'
   end
 end
