3d3387f83b4e28b7354b72db23f4350c303c63be
Kevin Sołtysiak <kevin@ksol.fr>
2013-08-23 09:40:58 +0200

    Default stack

--- lib/capistrano/novelys/core_extensions.rb
+++ lib/capistrano/novelys/core_extensions.rb
@@ -5,10 +5,11 @@ module Capistrano
       args.each { |recipes| load "novelys/#{recipes.to_s}" }
     end
 
-    # Sugar for loading common recipes + supplied
-    def use_default_stack_and(*args)
+    # Sugar for loading common recipes (+ supplied is present)
+    def use_default_stack(*args)
       args = ([:airbrake, :logs, :rbenv, :remote_commands, :unicorn] + args).uniq
       use_stack(*args)
     end
+    alias :use_default_stack_and :use_default_stack
   end
 end
