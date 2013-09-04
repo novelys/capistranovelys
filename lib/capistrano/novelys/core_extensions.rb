9b7ebffd361455762a2a7e92fdcdfcb5e4d9cde4
Kevin Sołtysiak <kevin@ksol.fr>
2013-09-04 08:59:20 +0200

    Bump to 0.5, finally allow dump_and_restore

--- lib/capistrano/novelys/core_extensions.rb
+++ lib/capistrano/novelys/core_extensions.rb
@@ -7,7 +7,7 @@ module Capistrano
 
     # Sugar for loading common recipes (+ supplied is present)
     def use_default_stack(*args)
-      args = ([:airbrake, :logs, :rbenv, :remote_commands, :stages, :unicorn] + args).uniq
+      args = ([:airbrake, :logs, :production_chain, :rbenv, :remote_commands, :stages, :unicorn] + args).uniq
       use_stack(*args)
     end
     alias :use_default_stack_and :use_default_stack
--- lib/capistrano/novelys/version.rb
+++ lib/capistrano/novelys/version.rb
@@ -1,5 +1,5 @@
 module Capistrano
   module Novelys
-    VERSION = '0.4.0'
+    VERSION = '0.5.0'
   end
 end
