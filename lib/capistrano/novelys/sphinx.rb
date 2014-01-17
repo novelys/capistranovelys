fda3ac42624c0d153dd93a226332defd36468932
Kevin Sołtysiak <kevin@ksol.fr>
2014-01-17 17:08:49 +0100

    Add missing sphinx recipes

--- lib/capistrano/novelys/sphinx.rb
+++ lib/capistrano/novelys/sphinx.rb
@@ -13,6 +13,46 @@ namespace :thinking_sphinx do
   task :symlink do
     run "test -f #{release_path}/config/#{rails_env}.sphinx.conf || ln -s #{shared_path}/config/#{rails_env}.sphinx.conf #{release_path}/config/#{rails_env}.sphinx.conf"
   end
+
+  desc "Generate the Sphinx configuration file"
+  task :configure do
+    Novelys::Helpers.run_remote_rake(self, "thinking_sphinx:configure")
+  end
+
+  desc "Index data"
+  task :index do
+    Novelys::Helpers.run_remote_rake(self, "thinking_sphinx:index")
+  end
+
+  desc "Start the Sphinx daemon"
+  task :start do
+    configure
+    Novelys::Helpers.run_remote_rake(self, "thinking_sphinx:start")
+  end
+
+  desc "Stop the Sphinx daemon"
+  task :stop do
+    configure
+    Novelys::Helpers.run_remote_rake(self, "thinking_sphinx:stop")
+  end
+
+  desc "Stop and then start the Sphinx daemon"
+  task :restart do
+    stop
+    start
+  end
+
+  desc "Stop, re-index and then start the Sphinx daemon"
+  task :rebuild do
+    stop
+    index
+    start
+  end
+
+  desc "Add the shared folder for sphinx files for the current environment"
+  task :shared_sphinx_folder, :roles => :web do
+    run "mkdir -p #{shared_path}/db/sphinx/#{rails_env}"
+  end
 end
 
 ## Alias for thinking_sphinx namespace
